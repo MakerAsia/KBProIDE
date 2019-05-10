48a8bd6369543e00cc3c330642915937d0c2fb4a
comdet <listzone@hotmail.com>
2019-05-10 13:31:12 +0700

    seperate update

--- src/App.vue
+++ src/App.vue
@@ -98,55 +98,7 @@
       {{ $global.ui.snackbarConfig.text }}
       <v-btn flat icon dark @click.native="$global.ui.snackbarStatus = false"><v-icon>close</v-icon></v-btn>
     </v-snackbar>
-
-    <!-- app updater -->
-    <v-dialog v-model="updateDialog" scrollable persistent max-width="500px">
-      <v-card>
-          <v-card-title>
-            <span class="headline">New version found!</span>
-          </v-card-title>
-          <v-divider></v-divider>
-          <v-card-text style="height: 300px;">
-            <template v-if="updateStatus == 'UPDATING'">
-              <div class="text-xs-center mt-3">
-              <v-progress-circular
-                :rotate="360"
-                :size="200"
-                :width="30"
-                :value="updateValue"
-                :indeterminate="updateValue < 0"
-                color="teal"
-              >
-                {{ updateValue }} %
-              </v-progress-circular>  
-              </div>
-              <div class="text-xs-center mt-4">
-              <span>{{updateText}}</span>
-              </div>
-            </template>
-            <template v-else-if="updateStatus == 'ERROR'">
-              <div class="text-xs-center mt-3">
-                <v-icon :size="200" color="red">fa-exclamation-triangle</v-icon>
-              </div> 
-            </template>
-            <template v-else>
-              <p v-html="update.info"></p>
-            </template>
-          </v-card-text>
-          <v-card-actions v-if="updateStatus != 'UPDATING'">
-            <v-spacer></v-spacer>
-            <v-tooltip top>
-              <template v-slot:activator="{ on }">
-                <v-btn color="blue darken-1" flat v-on="on" @click="ignoreUpdate(update.version)">Ignore this version</v-btn>
-              </template>
-              <span>Ignore this version but you can manually update by click 'help -> Update' on menubar</span>
-            </v-tooltip>
-            <v-btn color="blue darken-1" flat @click="updateDialog = false">Next time</v-btn>
-            <v-btn color="primary" @click="updateApp">Update Now!</v-btn>
-          </v-card-actions>
-      </v-card>
-    </v-dialog>
-    <!-- end update -->
+    <app-updater></app-updater>
   </div>
 </template>
 <script>
@@ -155,6 +107,8 @@ import AppToolbar from '@/engine/views/AppToolbar';
 import AppFooter from '@/engine/views/AppFooter';
 import { Multipane, MultipaneResizer } from 'vue-multipane';
 import draggable from 'vuedraggable'
+
+const electron = require('electron');
 //========= load manager ==========//
 import bm from '@/engine/BoardManager';
 import AsyncComponent from '@/engine/AsyncComponent';
@@ -163,8 +117,7 @@ import util from '@/engine/utils';
 import { stat } from 'fs';
 import { spread } from 'q';
 //========= updating =========//
-const EAU = require('electron-asar-hot-updater');
-const electron = require('electron');
+import AppUpdater from '@/engine/updater/AppUpdater';
 
 export default {
   components: {
@@ -173,18 +126,11 @@ export default {
     draggable,
     MultipaneResizer,
     AsyncComponent,
-    AppFooter
+    AppFooter,
+    AppUpdater
   },
   data: () => ({        
     expanded: true,
-    updateDialog : false,
-    update : {
-      info:'',
-      version:'',
-    },
-    updateStatus : 'IDLE',
-    updateValue : 0,
-    updateText : '',
   }),
   computed: {
 
@@ -201,13 +147,7 @@ export default {
     this.reloadBoardPackage();
     this.$global.$on('board-change',this.reloadBoardPackage);
     //----- check for update -----//
-    this.checkUpdate();
     this.$global.$on('check-update',this.checkUpdate);
-    electron.ipcRenderer.on('help-update',()=>
-    {
-      window.getApp.$dialog.notify.info('Checking new update'); 
-      this.checkUpdate(true,true); 
-    });
     electron.ipcRenderer.on('file-board-folder',()=>
     {
       electron.shell.openItem(util.boardDir);
@@ -224,85 +164,6 @@ export default {
   methods: {
     closeTab(name){
       this.$global.ui.removeAllTab(name);
-    },
-    checkUpdate(showNotification = true,forceShowUpdate = false){
-      this.$db.collection('apps').get().then(appData =>{    
-        if(appData.size == 1){
-          let data = appData.docs[0].data();
-          this.update = data;
-          //this.updateDialog = true;
-          EAU.init({
-            'server': false, // Where to check. true: server side, false: client side, default: true.
-            'debug': false // Default: false.
-          });
-          EAU.process(data,function (error, last, body) {
-            if(!error){
-              if(window.getApp.$global.setting.ignoreUpdateVersion == last && forceShowUpdate == false){
-                console.log('User ignored update popup');
-                return false
-              }
-              window.getApp.updateDialog = true;
-            }else if (error === 'no_update_available') { 
-              if(showNotification){
-                window.getApp.$dialog.notify.info('This is newest version');
-              }
-              return false;
-            }else{
-              window.getApp.$dialog.notify.error('check version error : ' + error);
-              return false;
-            }
-          });
-        }
-      });
-    },
-    ignoreUpdate(version){
-      this.$global.setting.ignoreUpdateVersion = version;
-      this.updateDialog = false;
-    },
-    updateApp(){
-      this.updateStatus = 'UPDATING';
-      this.updateText = 'Downloading ... ';
-      EAU.progress(function(state){
-        if(state.size.total){
-          window.getApp.updateValue = Math.round(state.percent * 100);
-          window.getApp.updateText = `Downloading ${util.humanFileSize(state.size.transferred)} of ${util.humanFileSize(state.size.total)}, speed : ${util.humanFileSize(state.speed)}/s`; 
-        }else{
-          window.getApp.updateText = `Downloading ${util.humanFileSize(state.size.transferred)} , speed : ${util.humanFileSize(speed)}/s`; 
-        }
-      });
-      /*EAU.progress(function (state) {
-        // The state is an object that looks like this:
-        // {
-        //     percent: 0.5,           
-        //     speed: 554732,
-        //     size: {
-        //         total: 90044871,        
-        //         transferred: 27610959   
-        //     },
-        //     time: {
-        //         elapsed: 36.235,        
-        //         remaining: 81.403       
-        //     }
-        // }
-      })*/
-
-      EAU.download(function (error) {
-        if (error) {
-          window.getApp.updateStatus = 'ERROR';
-          window.getApp.updateText = error;
-          setTimeout(()=>{
-            window.getApp.updateStatus = 'IDLE';
-          },2000);
-          return false;
-        }
-        //
-        console.log('Update success');
-        window.getApp.updateText = "Restarting ...";
-        setTimeout(()=>{
-          electron.remote.app.relaunch();
-          electron.remote.app.exit(0);
-        },2000);
-      })
     },
     reloadBoardPackage(){
       var boardName = this.$global.board.board;
