179f6ab2ce81d8126e61ef8cc2390c7112779960
comdet <listzone@hotmail.com>
2019-05-11 22:44:22 +0700

    add test

--- boards/kidbright-arduino/template.c
+++ boards/kidbright-arduino/template.c
@@ -1,17 +1,11 @@
 #include <Arduino.h>
 #include <vector>
 #include <WiFi.h>
-//#include <WiFiClient.h>
-//#include <WiFiAP.h>
-//#include <WebServer.h>
-//#include "SH1106.h"
-//#include "BME280.h"
-//#include "BMX055.h"
-//#include "BH1745NUC.h"
-//#include "KBProTime.h"
-//#include "BluetoothSerial.h"
-//#include "MusicDefinitions.h"
-//#include "XT_DAC_Audio.h"
+
+#include <Adafruit_GFX.h>
+#include "Adafruit_LEDBackpack.h"
+
+Adafruit_8x16minimatrix matrix = Adafruit_8x16minimatrix();
 
 ${EXTINC}
 
@@ -20,36 +14,111 @@ typedef int Boolean;
 
 using namespace std;
 
-//SH1106 display(0x3c, 21, 22);
-//BME280 bme280 = BME280();
-//BMX055 bmx055;
-//BH1745NUC bh1745;
-//KBProTime kbprotime;
-//BluetoothSerial SerialBT;
-//XT_DAC_Audio_Class DacAudio(26,3);
-
 ${VARIABLE}
 
 ${FUNCTION}
 
+
+
+static const uint8_t PROGMEM
+  smile_bmp[] =
+  { B00111100,
+    B01000010,
+    B10100101,
+    B10000001,
+    B10100101,
+    B10011001,
+    B01000010,
+    B00111100 },
+  neutral_bmp[] =
+  { B00111100,
+    B01000010,
+    B10100101,
+    B10000001,
+    B10111101,
+    B10000001,
+    B01000010,
+    B00111100 },
+  frown_bmp[] =
+  { B00111100,
+    B01000010,
+    B10100101,
+    B10000001,
+    B10011001,
+    B10100101,
+    B01000010,
+    B00111100 };
+
 void setup()
 {
-  pinMode(14,INPUT);
-  pinMode(15,INPUT);
-//  display.init();
-//  display.flipScreenVertically();
-//  display.setFont(ArialMT_Plain_10);
-//  //---SENSOR---//
-//  bme280.init();
-//  bmx055.begin();
-//  bh1745.getAddr_BH1745NUC(BH1745NUC_DEFAULT_ADDRESS);
-//  bh1745.Initialize();
-//  bh1745.begin();
+
   ${SETUP_CODE}
   ${BLOCKSETUP}
+
+  matrix.begin(0x70);
 }
 void loop()
 {
   ${LOOP_CODE}
   ${LOOP_EXT_CODE}
+
+  matrix.clear();
+  matrix.drawBitmap(0, 0, smile_bmp, 8, 8, LED_ON);
+  matrix.writeDisplay();
+  delay(500);
+  
+  matrix.clear();
+  matrix.drawBitmap(0, 8, neutral_bmp, 8, 8, LED_ON);
+  matrix.writeDisplay();
+  delay(500);
+
+  matrix.clear();
+  matrix.drawBitmap(0, 0, frown_bmp, 8, 8, LED_ON);
+  matrix.writeDisplay();
+  delay(500);
+
+
+  matrix.drawPixel(0, 0, LED_ON);  
+  matrix.writeDisplay();  // write the changes we just made to the display
+  delay(500);
+
+  matrix.clear();
+  matrix.drawLine(0,0, 7,15, LED_ON);
+  matrix.writeDisplay();  // write the changes we just made to the display
+  delay(500);
+
+  matrix.clear();
+  matrix.drawRect(0,0, 8,16, LED_ON);
+  matrix.fillRect(2,2, 4,12, LED_ON);
+  matrix.writeDisplay();  // write the changes we just made to the display
+  delay(500);
+
+  matrix.clear();
+  matrix.drawCircle(3,8, 3, LED_ON);
+  matrix.writeDisplay();  // write the changes we just made to the display
+  delay(500);
+
+  matrix.setTextSize(2);
+  matrix.setTextWrap(false);  // we dont want text to wrap so it scrolls nicely
+  matrix.setTextColor(LED_ON);
+  for (int8_t x=0; x>=-64; x--) {
+    matrix.clear();
+    matrix.setCursor(x,0);
+    matrix.print("Hello");
+    matrix.writeDisplay();
+    delay(100);
+  }
+
+  matrix.setTextSize(1);
+  matrix.setTextWrap(false);  // we dont want text to wrap so it scrolls nicely
+  matrix.setTextColor(LED_ON);
+  matrix.setRotation(1);
+  for (int8_t x=7; x>=-36; x--) {
+    matrix.clear();
+    matrix.setCursor(x,0);
+    matrix.print("World");
+    matrix.writeDisplay();
+    delay(100);
+  }
+  matrix.setRotation(0);
 }
