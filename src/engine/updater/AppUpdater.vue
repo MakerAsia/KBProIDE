ddf8ac28945633813531d9402c804e8ab8a965d8
comdet <listzone@hotmail.com>
2019-05-15 16:08:28 +0700

    update app updater

--- src/engine/updater/AppUpdater.vue
+++ src/engine/updater/AppUpdater.vue
@@ -52,62 +52,64 @@
 </template>
 
 <script>
-  const fs = require("fs");
-  import util from "@/engine/utils";
-  import Updater from "@/engine/updater/DownloadAndExtract";
+const fs = require("fs");
+import util from "@/engine/utils";
+import Updater from "@/engine/updater/DownloadAndExtract";
 
-  const EAU = require("electron-asar-hot-updater");
-  const electron = require("electron");
-  const requests = require("request-promise");
+const EAU = require("electron-asar-hot-updater");
+const electron = require("electron");
+const requests = require("request-promise");
 
-  var mother = null;
-  export default {
-    name: "app-updater",
-    components: {},
-    data() {
-      return {
-        updateDialog: false,
-        update: {
-          info: "",
-          version: "",
-        },
-        updateStatus: "IDLE",
-        updateValue: 0,
-        updateText: "",
-      };
-    },
-    created() {
-      mother = this;
-      this.checkUpdate();
-      electron.ipcRenderer.on("help-update", () => {
-        mother.$dialog.notify.info("Checking new update");
-        this.checkUpdate(true, true);
-      });
-    },
-    mounted() {
-
-    },
-    destroyed() {
-
-    },
-    methods: {
-      isOnline() {
-        return window.navigator.onLine;
+var mother = null;
+export default {
+  name: "app-updater",
+  components: {},
+  data() {
+    return {
+      updateDialog: false,
+      update: {
+        info: "",
+        version: ""
       },
-      checkUpdate(showNotification = false, forceShowUpdate = false) {
-        this.$db.collection("update").get().then(appData => {
+      updateStatus: "IDLE",
+      updateValue: 0,
+      updateText: ""
+    };
+  },
+  created() {
+    mother = this;
+    this.checkUpdate();
+    electron.ipcRenderer.on("help-update", () => {
+      mother.$dialog.notify.info("Checking new update");
+      this.checkUpdate(true, true);
+    });
+  },
+  mounted() {},
+  destroyed() {},
+  methods: {
+    isOnline() {
+      return window.navigator.onLine;
+    },
+    checkUpdate(showNotification = false, forceShowUpdate = false) {
+      this.$db
+        .collection("update")
+        .get()
+        .then(appData => {
           if (appData.size === 1) {
             let data = appData.docs[0].data();
             mother.update = data;
             if (this.update.type === "app") {
               //this.updateDialog = true;
               EAU.init({
-                         "server": false, // Where to check. true: server side, false: client side, default: true.
-                         "debug": false, // Default: false.
-                       });
+                server: false, // Where to check. true: server side, false: client side, default: true.
+                debug: false // Default: false.
+              });
               EAU.process(data, function(error, last, body) {
                 if (!error) {
-                  if (mother.$global.setting.ignoreUpdateVersion === last && forceShowUpdate === false) {
+                  if (
+                    mother.$global.setting.ignoreUpdateVersion === last &&
+                    forceShowUpdate === false
+                  ) {
                     console.log("User ignored update popup");
                     return false;
                   }
@@ -124,18 +126,29 @@
               });
             } else if (this.update.type === "platform") {
               Updater.init(this.update);
-              let pinfoFile = `${util.platformDir}/${mother.$global.board.board_info.platform}/config.js`;
-              if(!fs.existsSync(pinfoFile)){
+              let pinfoFile = `${util.platformDir}/${
+                mother.$global.board.board_info.platform
+              }/config.js`;
+              if (!fs.existsSync(pinfoFile)) {
                 return;
               }
-              let currentPlatformInfo = fs.readFileSync(pinfoFile);
-              if(mother.update.platform !== mother.$global.board.board_info.platform){
+              let currentPlatformInfo = eval(
+                fs.readFileSync(pinfoFile, "utf8")
+              );
+              if (
+                mother.update.platform !==
+                mother.$global.board.board_info.platform
+              ) {
                 return;
               }
-              if(mother.update.version <= currentPlatformInfo.version){
+              if (mother.update.version <= currentPlatformInfo.version) {
                 return;
               }
-              if (mother.$global.setting.ignorePlatformVersion === mother.update.version && forceShowUpdate === false) {
+              if (
+                mother.$global.setting.ignorePlatformVersion ===
+                  mother.update.version &&
+                forceShowUpdate === false
+              ) {
                 console.log("User ignored this platform update popup");
                 return;
               }
@@ -145,76 +158,84 @@
             }
           }
         });
-      },
-      ignoreUpdate(type,version) {
-        if(type === "app"){
-          this.$global.setting.ignoreUpdateVersion = version;
-        }else if(type === "platform"){
-          this.$global.setting.ignorePlatformVersion = version;
-        }else if(type === "board"){
-          //not support yet!
-        }
-        this.updateDialog = false;
-      },
-      updateApp() {
-        this.updateStatus = "UPDATING";
-        this.updateText = "Downloading ... ";
-        //======== app update ========//
-        if (this.update.type === "app") {
-          EAU.progress(mother.progress);
-          EAU.download(function(error) {
-            if (error) {
-              console.log("update app error : " + error);
-              mother.errorAndReset();
-              return false;
-            }
-            console.log("Update success");
-            mother.updateText = "Restarting ...";
-            setTimeout(() => {
-              electron.remote.app.relaunch();
-              electron.remote.app.exit(0);
-            }, 2000);
-          });
-        } else if (this.update.type === "platform") {
-          Updater.progress(mother.progress);
-          let currentPlatformDir = `${util.platformDir}/${mother.$global.board.board_info.platform}`;
-          Updater.process(currentPlatformDir).then(() => {
+    },
+    ignoreUpdate(type, version) {
+      if (type === "app") {
+        this.$global.setting.ignoreUpdateVersion = version;
+      } else if (type === "platform") {
+        this.$global.setting.ignorePlatformVersion = version;
+      } else if (type === "board") {
+        //not support yet!
+      }
+      this.updateDialog = false;
+    },
+    updateApp() {
+      this.updateStatus = "UPDATING";
+      this.updateText = "Downloading ... ";
+      //======== app update ========//
+      if (this.update.type === "app") {
+        EAU.progress(mother.progress);
+        EAU.download(function(error) {
+          if (error) {
+            console.log("update app error : " + error);
+            mother.errorAndReset(error);
+            return false;
+          }
+          console.log("Update success");
+          mother.updateText = "Restarting ...";
+          setTimeout(() => {
+            electron.remote.app.relaunch();
+            electron.remote.app.exit(0);
+          }, 2000);
+        });
+      } else if (this.update.type === "platform") {
+        Updater.progress(mother.progress);
+        let currentPlatformDir = `${util.platformDir}/${
+          mother.$global.board.board_info.platform
+        }`;
+        Updater.process(currentPlatformDir)
+          .then(() => {
             console.log("Update platform success");
             mother.updateText = "Reloading in 2 seconds ...";
             setTimeout(() => {
               document.location.reload();
             }, 2000);
-          }).catch(err => {
+          })
+          .catch(err => {
             console.log("update platform error : " + err);
-            mother.errorAndReset();
+            mother.errorAndReset(err);
             return false;
           });
-        }
-      },
-      progress(state) {
-        if (state.size.total) {
-          mother.updateValue = Math.round(state.percent * 100);
-          mother.updateText = `Downloading ${util.humanFileSize(state.size.transferred)} of ${util.humanFileSize(
-              state.size.total)}, speed : ${util.humanFileSize(state.speed)}/s`;
-        } else {
-          mother.updateText = `Downloading ${util.humanFileSize(
-              state.size.transferred)} , speed : ${util.humanFileSize(speed)}/s`;
-        }
-      },
-      errorAndReset(){
-        mother.updateStatus = "ERROR";
-        mother.updateText = error;
-        setTimeout(() => {
-          mother.updateStatus = "IDLE";
-        }, 2000);
       }
     },
-    watch: {
-      /*pluginDialog : function(val){
+    progress(state) {
+      if (state.size.total) {
+        mother.updateValue = Math.round(state.percent * 100);
+        mother.updateText = `Downloading ${util.humanFileSize(
+          state.size.transferred
+        )} of ${util.humanFileSize(
+          state.size.total
+        )}, speed : ${util.humanFileSize(state.speed)}/s`;
+      } else {
+        mother.updateText = `Downloading ${util.humanFileSize(
+          state.size.transferred
+        )} , speed : ${util.humanFileSize(speed)}/s`;
+      }
+    },
+    errorAndReset(error) {
+      mother.updateStatus = "ERROR";
+      mother.updateText = error;
+      setTimeout(() => {
+        mother.updateStatus = "IDLE";
+      }, 2000);
+    }
+  },
+  watch: {
+    /*pluginDialog : function(val){
           if(val){//on opening
               this.listOnlinePlugin();
           }
       }*/
-    },
-  };
+  }
+};
 </script>
