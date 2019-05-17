8dac03ff177521ba905fa2281a043150ee1d3ece
Nat <nat@cmmc.io>
2019-05-17 19:11:06 +0700

    bring config.js back.

---
+++ boards/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    data : {
+        board_config : 1,
+        block : {},
+        template : '',
+    },
+    persistence : {
+        board : 'kbpro', 
+        board_info : {
+            name : 'kbpro',
+            platform : 'arduino-esp32',
+            title : 'KB-Pro',
+            description : 'KB-Pro board for learn programming for everyone. บอร์ดสำหรับเรียนรู้การเขียนโปรแกรมสำหรับโปร มาพร้อมความสามารถที่มากขึ้น เช่น \n - หน่วยความจำ 4mb \n เซ็นเซอร์ Accerelometer Gyro meter\n',
+            author : 'Maker Asia',
+            website : 'http://www.kbpro.org',
+            email : 'comdet.p@gmail.com',
+            git : 'https://github.com/makerasia/kbpro/',
+            image : '/static/display.jpg',    
+            version : '1.0.0'
+        }
+    }
+}
