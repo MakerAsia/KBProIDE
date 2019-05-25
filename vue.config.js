e0cea5eb03a4a699ebf09161a698db9f4304a039
Nat <nat@cmmc.io>
2019-05-25 17:46:19 +0700

    update vue.config.js for bundle `boards` and `platforms`

--- vue.config.js
+++ vue.config.js
@@ -1,34 +1,70 @@
 module.exports = {
-    runtimeCompiler: true,
-    configureWebpack: {
-        externals: {
-            vue: 'Vue',
-            //vuetify: 'Vuetify'
+  runtimeCompiler: true,
+  configureWebpack: {
+    externals: {
+      vue: "Vue",
+      //vuetify: 'Vuetify'
+    },
+    /*entry: './resources/js/app.js',
+    output: {
+        filename: './public/javascripts/bundle.js',
+    },*/
+    /*resolve: {
+        alias: {
+          'vue$' : 'vue/dist/vue.esm.js',
+          'vuetify$' : 'vuetify/dist/vuetify.js'
+        }
+    },*/
+    /*resolve: {
+        alias: {
+            vue$ : 'vue/dist/vue.esm.js',
+            vuetify$: 'vuetify/dist/vuetify.js'
         },
-        /*entry: './resources/js/app.js',
-        output: {
-            filename: './public/javascripts/bundle.js',
-        },*/
-        /*resolve: {
-            alias: {
-              'vue$' : 'vue/dist/vue.esm.js',
-              'vuetify$' : 'vuetify/dist/vuetify.js'
+        extensions: ['*', '.js', '.vue', '.json']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
             }
-        },*/
-        /*resolve: {
-            alias: {
-                vue$ : 'vue/dist/vue.esm.js',
-                vuetify$: 'vuetify/dist/vuetify.js'
-            },
-            extensions: ['*', '.js', '.vue', '.json']
+        ]
+    }*/
+  },
+  pluginOptions: {
+    electronBuilder: {
+      outputDir: "dist_electron",
+      builderOptions: {
+        appId: "com.makerasia.ide",
+        "mac": {
+          "category": "public.app-category.productivity",
         },
-        module: {
-            rules: [
-                {
-                    test: /\.vue$/,
-                    loader: 'vue-loader'
-                }
-            ]
-        }*/
-    }
-}
+        "dmg": {},
+        "copyright": "Copyright © 2019 MakerAsia Co.,Ltd",
+        extraResources: [
+          {
+            "from": "boards",
+            "to": "../boards",
+          },
+          {
+            "from": "platforms",
+            "to": "../platforms",
+          },
+        ],
+        // options placed here will be merged with default configuration and passed to electron-builder
+      },
+      chainWebpackMainProcess: config => {
+        // Chain webpack config for electron main process only
+        console.log("chaingWebpackMainProcess");
+      },
+      chainWebpackRendererProcess: config => {
+        // Chain webpack config for electron renderer process only
+        // The following example will set IS_ELECTRON to true in your app
+        config.plugin("define").tap(args => {
+          args[0]["IS_ELECTRON"] = true;
+          return args;
+        });
+      },
+    },
+  },
+};
