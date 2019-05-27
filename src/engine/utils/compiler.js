9f1cdafc481f559a54000cc2bb441edfdaac1271
Nat <nat@cmmc.io>
2019-05-28 05:14:20 +0700

    update submodule.

--- src/engine/utils/compiler.js
+++ src/engine/utils/compiler.js
@@ -3,8 +3,8 @@ export default {
     return new Promise((resolve, reject) => {
       let errors = [];
       if (err.error) {
-        errors = err.error.stderr.split("\n");
-        errors = errors.filter(v => v.indexOf("user_app.cpp") > -1).
+        errors = err.error.stderr.split("\n").
+        filter(v => v.indexOf("user_app.cpp") > -1).
         map(v => v.split("user_app.cpp:")[1]);
         setTimeout(() => {
           resolve(errors);
