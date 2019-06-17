5c266371d388618fca466b1be9251be8ad8af59b
Nat <nat@cmmc.io>
2019-06-18 00:45:55 +0700

    execute  vue add @vue/unit-jest

---
+++ test/unit/specs/example.spec.js
@@ -0,0 +1,14 @@
+//import {shallowMount} from '@vue/test-utils';
+//import HelloWorld from '@/components/HelloWorld.vue';
+//var menu = require('../../../src/tour');
+import '../../../src/tour';
+
+describe('HelloWorld.vue', () => {
+  it('renders props.msg when passed', () => {
+    //const msg = 'new message'; const wrapper = shallowMount(HelloWorld, {
+    //  propsData: {msg},
+    //});
+    //expect(wrapper.text()).toMatch(msg);
+    expect('a').toMatch('a');
+  });
+});
