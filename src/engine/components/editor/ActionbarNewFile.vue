67713e464cb91f2efd260a4571f7b47322ecc9e7
Prawared Bowonphattharawadi <prawared.bw@gmail.com>
2019-09-16 14:10:21 +0700

    - Reformat Code - Fixed bug new file when raw code is on

--- src/engine/components/editor/ActionbarNewFile.vue
+++ src/engine/components/editor/ActionbarNewFile.vue
@@ -17,20 +17,28 @@
     },
     methods: {
       newFile: async function() {
-        if (this.$global.editor.mode < 3) {
+        if (this.$global.editor.mode < 3 || this.$store.state.rawCode.mode === true) {
           const res = await this.$dialog.confirm({
-                                                   text: "Do you really want to clear this workspace?",
-                                                   title: "Warning",
-                                                   actions: [
-                                                     {text: "Confirm", key: "confirm"},
-                                                     {text: "Cancel", key: false, color: "red darken-1"}
-                                                   ]
-                                                 });
+            text: "Do you really want to clear this workspace?",
+            title: "Warning",
+            actions: [
+              { text: "Confirm", key: "confirm" },
+              { text: "Cancel", key: false, color: "red darken-1" }
+            ]
+          });
 
           if (res) {
             if (res === "confirm") {
               this.$global.editor.blockCode = "";
               this.$global.$emit("editor-mode-change", this.$global.editor.mode); //mode 1 no need to convert code
+
+              if (this.$store.state.rawCode.mode === true) {
+                this.$global.editor.mode = 2;
+                this.$global.$emit("editor-mode-change", 2);
+                this.$global.editor.mode = 3;
+                this.$global.$emit("editor-mode-change", 3, true);
+              }
+
             }
           }
         } else {
@@ -43,7 +51,7 @@
               { text: "Cancel", key: false, color: "red darken-1" }
             ]
           });
-          if (res && res !== 'Cancel') {
+          if (res && res !== "Cancel") {
             if (res === "convert") {
               this.$global.$emit("editor-mode-change", this.$global.editor.mode, true);
             } else {
