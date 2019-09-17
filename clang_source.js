585897e4953f9d30b81517273cd2772daf1a6048
Prawared Bowonphattharawadi <prawared.bw@gmail.com>
2019-09-17 12:39:16 +0700

    set empty file

--- clang_source.js
+++ clang_source.js
@@ -4,56 +4,19 @@
 #include <WiFiAP.h>
 #include <WebServer.h>
 
-#include <Wire.h>
-#include <RtcDS1307.h>
 
-RtcDS1307<TwoWire> RTC1(Wire);
+
+
 
 
 
 void setup()
 {
-  RTC1.Begin();
-    RtcDateTime RTC1_compiled = RtcDateTime(__DATE__, __TIME__);
-    // printDateTime(RTC1_compiled);
-
-    if (!RTC1.IsDateTimeValid())
-    {
-      if (RTC1.LastError() != 0)
-      {
-
-      }
-      else
-      {
-        RTC1.SetDateTime(RTC1_compiled);
-      }
-    }
-
-    if (!RTC1.GetIsRunning())
-    {
-      RTC1.SetIsRunning(true);
-    }
-
-    RtcDateTime RTC1_now = RTC1.GetDateTime();
-    if (RTC1_now < RTC1_compiled)
-    {
-      RTC1.SetDateTime(RTC1_compiled);
-    }
-    else if (RTC1_now > RTC1_compiled)
-    {
-
-    }
-    else if (RTC1_now == RTC1_compiled)
-    {
-
-    }
-
-    RTC1.SetSquareWavePin(DS1307SquareWaveOut_Low);
-  pinMode(25,OUTPUT);
+  
+  
 }
 void loop()
 {
-    delay(500);
-
+  
   
 }
