9c93bad4e5c27c9beffe38112385bb32626cb9d8
Prawared Bowonphattharawadi <prawared.bw@gmail.com>
2019-10-24 11:25:18 +0700

    Vuex store update

--- src/store.js
+++ src/store.js
@@ -3,34 +3,79 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+const market = {
+  cart: []
+}
+
 export default new Vuex.Store({
   state: {
-  	rawCode: {
-  		mode: false,
-  		rollbackMode: 0,
-  		display: false,
-  	}
+    ...market
   },
   mutations: {
-  	rawCodeMode(state, data) {
-    	state.rawCode.mode = data;
+    addToCart(state, data) {
+      let found = state.cart.find(product => product.id === data.id);
+
+      if (found) {
+        found.quantity++;
+        found.totalPrice = (parseInt(found.price) * found.quantity);
+        state.subTotal = state.subTotal + parseInt(found.price);
+      } else {
+        data.quantity = 1;
+        data.totalPrice = parseInt(data.price);
+        state.subTotal = state.subTotal + parseInt(data.price);
+        state.cart.push(data);
+        state.cartCount++;
+      }
     },
-    rollbackRawCode(state, data) {
-    	state.rawCode.rollbackMode = data;
+    removeQuantityToCart(state, data) {
+      let found = state.cart.find(product => product.id === data.id);
+
+      if (found) {
+        if (found.quantity === 1) {
+          // remove product from cart
+          let index = state.cart.indexOf(data);
+
+          if (index > -1) {
+            let product = state.cart[index];
+            let quantity = parseInt(product.quantity);
+            let price = parseInt(product.price) * quantity;
+
+            state.cartCount -= product.quantity;
+            state.cart.splice(index, 1);
+
+            state.subTotal = state.subTotal - price;
+          }
+        } else {
+          found.quantity--;
+          found.totalPrice = (parseInt(found.price) * found.quantity);
+          state.subTotal = state.subTotal - parseInt(found.price);
+        }
+      }
     },
-    rawCodeToggleDisplay(state, data) {
-    	state.rawCode.display = data;
-    }
+    removeFromCart(state, data) {
+      let index = state.cart.indexOf(data);
+
+      if (index > -1) {
+        let product = state.cart[index];
+        let quantity = parseInt(product.quantity);
+        let price = parseInt(product.price) * quantity;
+
+        state.cartCount -= product.quantity;
+        state.cart.splice(index, 1);
+
+        state.subTotal = state.subTotal - price;
+      }
+    },
   },
   actions: {
-  	rawCodeMode(context, data) {
-    	context.commit('rawCodeMode', data);
+    addToCart(context, data) {
+      context.commit('addToCart', data);
     },
-    rollbackRawCode(context, data) {
-    	context.commit('rollbackRawCode', data);
+    removeQuantityToCart(context, data) {
+      context.commit('removeQuantityToCart', data);
     },
-    rawCodeToggleDisplay(context, data) {
-    	context.commit('rawCodeToggleDisplay', data);
+    removeFromCart(context, data) {
+      context.commit('removeFromCart', data);
     },
   }
 });
