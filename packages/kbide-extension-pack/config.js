2306e623c53dc4e80de55f58adc90cf64eea3ef0
comdet <listzone@hotmail.com>
2019-12-02 11:44:26 +0700

    rename serial monitor to kbide-extension-pack.

--- packages/kbide-extension-pack/config.js
+++ packages/kbide-extension-pack/config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  name: "kbide-serial-monitor",
-  title: "KBIDE Serial Monitor",
-  description: "Serial monitor",
-  auther: "Comdet Pheaudphut",
+  name: "kbide-extension-pack",
+  title: "KBIDE Extensions Pack",
+  description: "KBIDE Tools (Packed) Ex. Serial Monitor, Developer code preview, C/C++ Code auto format",
+  auther: "Maker Asia Co.,Ltd.",
   website: "kbide.org",
   git: "",
-  image: "",
+  image: "/static/extension.jpg",
   version: "1.0.0",
   component: [
     "serial-monitor",
