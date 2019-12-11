69545a6b50cf8edfa1679377bf9efc61f8ee3cce
comdet <listzone@hotmail.com>
2019-12-11 15:36:31 +0700

    fix build error.

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,8 @@
-const ClosurePlugin = require('closure-webpack-plugin');
 module.exports = {
   runtimeCompiler: true,
   configureWebpack: {
     optimization: {
-      minimizer: [
-        new ClosurePlugin({mode: 'STANDARD'}, {
-          // compiler flags here
-          //
-          // for debuging help, try these:
-          //
-          // formatting: 'PRETTY_PRINT'
-          // debug: true,
-          // renaming: false
-        })
-      ]
+
     },
     externals: {
       vue: "Vue",
